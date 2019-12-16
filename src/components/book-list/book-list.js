82c88f4a3dd4b3bd3da50a5adf3c8511d469e173
Paul7967 <postatochnikov@yandex.ru>
2019-12-16 18:28:52 +0500

    Change <Spinner /> component

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,28 +5,37 @@ import { connect } from 'react-redux';
 
 import './book-list.css';
 import withBookstoreService from './../hoc';
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks, bookAddedToCart } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
 import ErrorIndicator from './../error-indicator';
 
-class BookList extends Component {
+const BookList = ({ books, onAddedToCart }) => {
+	return (
+		<ul className="book-list">
+			{
+			books.map((book) => {
+				return (
+					<li key={book.id}>
+						<BookListItem 
+							onAddedToCart={() => onAddedToCart(book.id)}
+							book={book} />
+					</li>
+				)	
+			})
+			}
+		</ul>
+	)
+};
+
+class BookListContainer extends Component {
 
 	componentDidMount() {
-		const { 
-			bookstoreService, 
-			booksLoaded, 
-			booksRequested, 
-			booksError } = this.props;
-
-		booksRequested();
-		bookstoreService.getBooks()					// 1. receive data
-			.then((data) => booksLoaded(data))      // 2. dispatch action to store
-			.catch((err) => booksError(err));		// Handle error
+		this.props.fetchBooks();
 	}			
 
 	render() {
-		const { books, loading, error } = this.props;
+		const { books, loading, error, onAddedToCart } = this.props;
 
 		if (loading) {
 			return <Spinner />
@@ -37,16 +46,9 @@ class BookList extends Component {
 		}
 
 		return (
-			<ul className="book-list">
-				{
-				books.map((book) => {
-					return (
-						<li key={book.id}><BookListItem book={book} /></li>
-					)	
-				})
-				}
-			</ul>
+			<BookList books={books} onAddedToCart={onAddedToCart} />
 		)
+		
 	}
 }
 
@@ -54,14 +56,16 @@ const mapStateToProps = ({ books, loading, error }) => {
 	return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-	booksLoaded,
-	booksRequested,
-	booksError
-}
+const mapDispatchToProps = (dispatch, ownProps) => {
+	const { bookstoreService } = ownProps;
+	return {
+		fetchBooks: fetchBooks(bookstoreService, dispatch),
+		onAddedToCart: (id) => dispatch(bookAddedToCart(id))
+	};
+};
 
 export default compose(
 	withBookstoreService(),
 	connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,77 @@
 const initialState = {
 	books: [],
 	loading: true,
-	error: null
+	error: null,
+	cartItems: [],
+	total: 220
 }
 
 const reducer = (state= initialState, action) => {
 	
 	switch (action.type) {
-		case 'BOOKS_REQUESTED':
+		case 'FETCH_BOOKS_REQUEST':
 			return {
+				...state,
 				books: [],
 				loading: true,
 				error: null
 			};
-		case 'BOOKS_LOADED':
+		case 'FETCH_BOOKS_SUCCESS':
 			return {
+				...state,
 				books: action.payload,
 				loading: false,
 				error: null
 			};
-		case 'BOOKS_ERROR':
+		case 'FETCH_BOOKS_FAILURE':
 			return {
+				...state,
 				books: [],
 				loading: false,
 				error: action.payload
 			}
+
+		case 'BOOK_ADDED_TO_CART':
+			const bookId = action.payload;
+			const book = state.books.find((book) => book.id === bookId );
+			const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
+			const item = state.cartItems[itemIndex];
+			
+			let newItem ;
+			if (item) {
+				newItem = {
+					...item,
+					count: item.count + 1,
+					total: item.total + book.price
+			}; 
+			} else {
+				newItem = {
+					id: book.id,
+					title: book.title,
+					count: 1,
+					total: book.price
+				};
+			};	
+
+			if (itemIndex<0) {
+				return {
+					...state,
+					cartItems: [
+						...state.cartItems,
+						newItem]
+				};
+			} else {
+				// console.log({state.cartItems[id]});
+				return {
+					...state,
+					cartItems: [
+						...state.cartItems.slice(0, itemIndex),
+						newItem,
+						...state.cartItems.slice(itemIndex + 1)]
+				};
+			};
+			
+
 
 		default:
 			return state;
